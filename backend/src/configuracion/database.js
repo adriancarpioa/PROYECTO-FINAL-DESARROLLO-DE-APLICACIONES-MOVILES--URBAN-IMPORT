e8c368feaf353dbd879362d64fc803eb8fe94df2
adriancarpioa <adrianlcarpio@gmail.com>
2026-07-16 19:57:16 -0500

    Debug variables Render

--- backend/src/configuracion/database.js
+++ backend/src/configuracion/database.js
@@ -4,6 +4,12 @@ const path = require('path');
 
 require('dotenv').config();
 
+
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_NAME:', process.env.DB_NAME);
+
 const configuracion = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
